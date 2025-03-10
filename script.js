e29d7d291e70a0583e70216f0aba93aa65b47dbc
João Tavares <32026323+joaorstavares@users.noreply.github.com>
2025-03-10 18:31:31 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/*
+//type conversion
+const inputYear = '1991';
+
+console.log(Number(inputYear)+18);
+console.log(inputYear+18);
+console.log(Number('Jonas'));
+console.log(String(23),Number(inputYear));
+
+//type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23'+'10'+3);
+console.log('23'*'10');
+console.log('23'/'10');
+
+let n = '1'+1;
+n = n - 1
+console.log(n);
+
+n = 2+3+4+'5';
+console.log(n);
+
+n = '10'-'4'-'3'-2+'5';
+console.log(n);
+*/
+
+//5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
